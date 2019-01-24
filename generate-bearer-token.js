dc973cb243501aceee8a4409e2579715b518f945
Shohil Kishore <33049612+shohil-kishore@users.noreply.github.com>
2019-01-24 14:58:35 +1300

    Refactored generate-bearer-token.js

--- generate-bearer-token.js
+++ generate-bearer-token.js
@@ -1,36 +1,48 @@
-// Install Node.js and the request package before running.
-// 'npm install request' 
-// Code implements steps from here: https://developer.twitter.com/en/docs/basics/authentication/overview/application-only.html
+/* 
+This code allows users to generate a bearer token.
 
-// Imports.
-var request = require('request');
+generateBearerToken requires two parameters (Consumer Key and Consumer Secret Key) which can be obtained from Twitter.
 
-// Insert key/secret constants here. This is all that needs to be changed.
-var CONSUMER_KEY = '';
-var CONSUMER_SECRET_KEY = '';
+More information can be found here: https://developer.twitter.com/en/docs/basics/authentication/overview/application-only.html
+*/
 
-// Encode key and secret key.
-var ENCODED_KEY = new Buffer(CONSUMER_KEY + ':' + CONSUMER_SECRET_KEY).toString('base64');
+// Replace each string with the relevant key. Enclose each key in quotation marks as below.
+generateBearerToken("CONSUMER_KEY", "CONSUMER_SECRET_KEY");
 
-// As per documentation.
-var options = {
-    url: 'https://api.twitter.com/oauth2/token',
+function generateBearerToken(consumerKey, consumerSecretKey) {
+  var request = require("request");
+
+  // Assign key and secret key.
+  var CONSUMER_KEY = consumerKey;
+  var CONSUMER_SECRET_KEY = consumerSecretKey;
+
+  // Encode key and secret key.
+  var ENCODED_KEY = new Buffer(
+    CONSUMER_KEY + ":" + CONSUMER_SECRET_KEY
+  ).toString("base64");
+
+  // Configure request.
+  var options = {
+    url: "https://api.twitter.com/oauth2/token",
     headers: {
-        'Authorization': 'Basic ' + ENCODED_KEY,
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-    body: 'grant_type=client_credentials'
-};
+      Authorization: "Basic " + ENCODED_KEY,
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    },
+    body: "grant_type=client_credentials"
+  };
 
-// A string is returned from the request, so the body must be parsed. 
-request.post(options, function(error, response, body) {
+  // Request returns string, so it must be parsed.
+  request.post(options, function(error, response, body) {
     body = JSON.parse(body);
 
-    // Basic error handling.
+    // Returning the key or handling the error.
     if (body.token_type === "bearer") {
-      console.log("Success! Bearer token:");
+      console.log("Success! Bearer token: ");
       console.log(body.access_token);
+      return body.access_token;
     } else {
       console.log("Sorry, something went wrong. Here's the error: ");
-      console.log(error); 
+      console.log(error);
     }
-});
+  });
+}
