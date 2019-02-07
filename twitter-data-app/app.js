3ec5d7ee57f1b5af07291f3b016e95a202925e8d
shohil-kishore <s.kishore@auckland.ac.nz>
2019-02-07 16:55:19 +1300

    Added initial route

--- twitter-data-app/app.js
+++ twitter-data-app/app.js
@@ -1,10 +1,21 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/generate-token", (req, res) => {
+  var secretKey = req.query.secret;
+  var publicKey = req.query.public;
+  res.render("token-generator.ejs", {
+    secretKey: secretKey,
+    publicKey: publicKey
+  });
 });
 
 app.listen(3000, () => {
