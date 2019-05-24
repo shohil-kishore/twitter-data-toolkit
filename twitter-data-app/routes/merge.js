0deca11c2186719ecea3ffa893d6deab3ba370fb
shohil-kishore <s.kishore@auckland.ac.nz>
2019-05-24 15:54:17 +1200

    Completed merge route

--- twitter-data-app/routes/merge.js
+++ twitter-data-app/routes/merge.js
@@ -1,8 +1,7 @@
 // Import packages.
 const express = require("express");
 const router = express.Router();
-const concat = require("json-concat");
-const fs = require('fs-extra')
+const fs = require("fs-extra");
 
 // Merge GET route.
 router.get("/merge", (req, res) => {
@@ -11,23 +10,41 @@ router.get("/merge", (req, res) => {
 
 // Merge POST route.
 router.post("/merge", (req, res) => {
-  
-  // Loop through file, add each JS object to a new file.
-  concat(
-    {
-      src: "../data",
-      dest: "../result.json"
-    },
-    () => {
-      console.log("JSON files combined successfully.");
-      // Empty data directory.
-      fs.emptyDir("../data", err => {
-        if (err) return console.error(err);
-        console.log("Data directory emptied successfully (data still exists in backup-data folder).");
+  mergeAndDeleteFiles();
+
+  // Merge files in the correct format.
+  function mergeAndDeleteFiles() {
+    // Generate array of paths to JSON files to use for processing.
+    var files = [];
+    var dir = "../data/";
+    fs.readdirSync(dir).forEach(file => {
+      files.push(dir + file);
+    });
+
+    // Process each file indivdually. Read, parse into JSON, remove irrelevant characters and append to a file in the correct format.
+    for (let i = 0; i < files.length; i++) {
+      let raw = fs.readFileSync(files[i]);
+      let json = JSON.parse(raw);
+      // Remove characters that invalidate JSON format.
+      let processedData = JSON.stringify(json)
+        .substr(1)
+        .slice(0, -1);
+      // Add characters that validate JSON format.
+      if (i === 0) {
+        processedData = "[" + processedData + ",";
+      } else if (i === files.length - 1) {
+        processedData = processedData + "]";
+      } else {
+        processedData = processedData + ",";
+      }
+      // Append to an existing file. File should be cleared at some stage.
+      fs.appendFile("result.json", processedData, err => {
+        if (err) throw err;
       });
-      res.render("merge");
     }
-  );
+    console.log("Complete data file generated successfully.");
+    res.render("merge");
+  }
 });
 
 // Output location of complete file.
