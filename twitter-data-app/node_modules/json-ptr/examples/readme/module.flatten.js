4d29605a7f82366553f983f265abb90bdc5a20fc
shohil-kishore <s.kishore@auckland.ac.nz>
2020-11-09 11:41:23 +1300

    Remove .gitignore files

--- twitter-data-app/node_modules/json-ptr/examples/readme/module.flatten.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var ptr = require('../../'); // json-ptr
-var data = require('./data');
-
-// var obj = ptr.flatten(data);
-var obj = ptr.flatten(data, true);
-
-console.log(JSON.stringify(obj, null, '  '));
--- twitter-data-app/node_modules/json-ptr/examples/readme/module.list.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var ptr = require('../../'); // json-ptr
-var data = require('./data');
-
-// var list = ptr.list(data);
-var list = ptr.list(data, true);
-
-console.log(JSON.stringify(list, null, '  '));
